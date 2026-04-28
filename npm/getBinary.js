10a4e8d4ec1a23ca7e7f949ba15d963abfa7d9bf
Gyanendra Thakur <gyanendrathakur4135stella@gmail.com>
2026-04-28 10:32:09 +0530

    fix: binary URL template + release workflow permissions

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -1,38 +1,32 @@
-const { Binary } = require("binary-install");
-const pkg = require("../package.json");
+// npm/getBinary.js
+const { Binary } = require('binary-install');
+const pkg = require('../package.json');
 
 const PLATFORM_MAP = {
-  darwin: "macos",
-  linux: "linux",
-  win32: "windows",
+  darwin: 'macos',
+  linux:  'linux',
+  win32:  'windows',
 };
 
 const ARCH_MAP = {
-  x64: "x86_64",
-  arm64: "aarch64",
-  ia32: "i686",
+  x64:   'x86_64',
+  arm64: 'aarch64',
 };
 
-function getPlatform() {
+function getBinary() {
   const platform = PLATFORM_MAP[process.platform];
-  const arch = ARCH_MAP[process.arch];
+  const arch     = ARCH_MAP[process.arch];
 
   if (!platform || !arch) {
     throw new Error(`Unsupported platform: ${process.platform} ${process.arch}`);
   }
 
-  return { platform, arch };
-}
+  const version  = pkg.version;  // "0.1.0"
+  const name     = `nexus_${version}_${platform}_${arch}.tar.gz`;
+  const url      = `https://github.com/GTgyani206/nexus/releases/download/v${version}/${name}`;
 
-function getBinary() {
-  const { platform, arch } = getPlatform();
-  const version = pkg.version;
-  const url = pkg.binary.url
-    .replace("{{version}}", version)
-    .replace("{{platform}}", platform)
-    .replace("{{arch}}", arch);
-
-  return new Binary(pkg.binary.name, url);
+  console.log(`Downloading: ${url}`);
+  return new Binary('nexus', url);
 }
 
 module.exports = getBinary;
